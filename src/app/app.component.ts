18e5eb5a3992f81663502ac5292f1baeb2dec1bc
henning-nr <115693958+henning-nr@users.noreply.github.com>
2023-04-20 20:58:38 -0300

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,117 +3,153 @@ import { Product } from './product';
 import { ProductService } from './productservice';
 import { ConfirmationService } from 'primeng/api';
 import { MessageService } from 'primeng/api';
+import { Pessoas } from './pessoas';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-    styles: [`
+  styles: [
+    `
         :host ::ng-deep .p-dialog .product-image {
             width: 150px;
             margin: 0 auto 2rem auto;
             display: block;
         }
-    `],
-    styleUrls: ['./app.component.scss']
+    `,
+  ],
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent { 
-    productDialog: boolean;
+export class AppComponent {
+  productDialog: boolean;
 
-    products: Product[];
+  products: Product[];
 
-    product: Product;
+  nameButton = 'Adicionar';
 
-    selectedProducts: Product[];
+  product: Product;
 
-    submitted: boolean;
+  selectedProducts: Product[];
 
-    constructor(private productService: ProductService, private messageService: MessageService, private confirmationService: ConfirmationService) { }
+  submitted: boolean;
 
-    ngOnInit() {
-        this.productService.getProducts().then(data => this.products = data);
-    }
+  constructor(
+    private productService: ProductService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
+  ) {}
 
-    openNew() {
+  ngOnInit() {
+    this.productService.getProducts().then((data) => {
+      this.products = data;
+    });
+    this.productService.getPessoas().then((data) => {
+      console.log('pessaos', data);
+    });
+  }
+
+  openNew() {
+    this.product = {};
+    this.submitted = false;
+    this.productDialog = true;
+  }
+
+  deleteSelectedProducts() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete the selected products?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.products = this.products.filter(
+          (val) => !this.selectedProducts.includes(val)
+        );
+        this.selectedProducts = null;
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Products Deleted',
+          life: 3000,
+        });
+      },
+    });
+  }
+
+  editProduct(product: Product) {
+    this.product = { ...product };
+    this.productDialog = true;
+  }
+
+  deleteProduct(product: Product) {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete ' + product.name + '?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.products = this.products.filter((val) => val.id !== product.id);
         this.product = {};
-        this.submitted = false;
-        this.productDialog = true;
-    }
-
-    deleteSelectedProducts() {
-        this.confirmationService.confirm({
-            message: 'Are you sure you want to delete the selected products?',
-            header: 'Confirm',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.products = this.products.filter(val => !this.selectedProducts.includes(val));
-                this.selectedProducts = null;
-                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Products Deleted', life: 3000});
-            }
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Product Deleted',
+          life: 3000,
         });
-    }
+      },
+    });
+  }
 
-    editProduct(product: Product) {
-        this.product = {...product};
-        this.productDialog = true;
-    }
+  hideDialog() {
+    this.productDialog = false;
+    this.submitted = false;
+  }
 
-    deleteProduct(product: Product) {
-        this.confirmationService.confirm({
-            message: 'Are you sure you want to delete ' + product.name + '?',
-            header: 'Confirm',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.products = this.products.filter(val => val.id !== product.id);
-                this.product = {};
-                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Deleted', life: 3000});
-            }
+  saveProduct() {
+    this.submitted = true;
+
+    if (this.product.name.trim()) {
+      if (this.product.id) {
+        this.products[this.findIndexById(this.product.id)] = this.product;
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Sucesso',
+          detail: 'Produto Atualizado',
+          life: 3000,
         });
+      } else {
+        this.product.id = this.createId();
+        this.product.image = 'product-placeholder.svg';
+        this.products.push(this.product);
+        this.messageService.add({
+          severity: 'danger',
+          summary: 'Sucesso',
+          detail: 'Produto Salvo',
+          life: 3000,
+        });
+      }
+
+      this.products = [...this.products];
+      this.productDialog = false;
+      this.product = {};
+    }
+  }
+
+  findIndexById(id: string): number {
+    let index = -1;
+    for (let i = 0; i < this.products.length; i++) {
+      if (this.products[i].id === id) {
+        index = i;
+        break;
+      }
     }
 
-    hideDialog() {
-        this.productDialog = false;
-        this.submitted = false;
+    return index;
+  }
+
+  createId(): string {
+    let id = '';
+    var chars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < 5; i++) {
+      id += chars.charAt(Math.floor(Math.random() * chars.length));
     }
-    
-    saveProduct() {
-        this.submitted = true;
-
-        if (this.product.name.trim()) {
-            if (this.product.id) {
-                this.products[this.findIndexById(this.product.id)] = this.product;                
-                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Updated', life: 3000});
-            }
-            else {
-                this.product.id = this.createId();
-                this.product.image = 'product-placeholder.svg';
-                this.products.push(this.product);
-                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Created', life: 3000});
-            }
-
-            this.products = [...this.products];
-            this.productDialog = false;
-            this.product = {};
-        }
-    }
-
-    findIndexById(id: string): number {
-        let index = -1;
-        for (let i = 0; i < this.products.length; i++) {
-            if (this.products[i].id === id) {
-                index = i;
-                break;
-            }
-        }
-
-        return index;
-    }
-
-    createId(): string {
-        let id = '';
-        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        for ( var i = 0; i < 5; i++ ) {
-            id += chars.charAt(Math.floor(Math.random() * chars.length));
-        }
-        return id;
-    }
+    return id;
+  }
 }
---
+++ src/app/pessoas.ts
@@ -0,0 +1,26 @@
+export interface Pessoas {
+    id: number;
+    nome: string;
+    idade: number;
+    cpf: string;
+    rg: string;
+    data_nasc: string;
+    sexo: string;
+    signo: string;
+    mae: string;
+    pai: string;
+    email: string;
+    senha: string;
+    cep: string;
+    endereco: string;
+    numero: number;
+    bairro: string;
+    cidade: string;
+    estado: string;
+    telefone_fixo: string;
+    celular: string;
+    altura: string;
+    peso: number;
+    tipo_sanguineo: string;
+    cor: string;
+  }
